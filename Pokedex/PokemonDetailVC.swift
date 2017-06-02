54c711c4dfab792107e136a33922918b98abe36b
Matt Tripodi <mtripodi1027@gmail.com>
2017-06-02 13:11:20 -0400

    All of the UI is now updating with the pokemon data

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -29,8 +29,13 @@ class PokemonDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		
-		nameLbl.text = pokemon.name
+		nameLbl.text = pokemon.name.capitalized
 		
+		let img = UIImage(named: "\(pokemon.pokedeId)")
+		
+		mainImg.image = img
+		currentEvoImg.image = img
+		pokedexLbl.text = "\(pokemon.pokedeId)"
 		pokemon.downloadPokemonDetails {
 			
 			// Whatever we write here will only be called after the network call is complete!
@@ -45,6 +50,21 @@ class PokemonDetailVC: UIViewController {
 		defenseLbl.text = pokemon.defense
 		heightLbl.text = pokemon.height
 		weightLbl.text = pokemon.weight
+		typeLbl.text = pokemon.type
+		descriptionLbl.text = pokemon.description
+		
+		if pokemon.nextEvolutionId == "" {
+			
+			evoLbl.text = "No Evolutions"
+			nextEvoImg.isHidden = true
+			
+		} else {
+			
+			nextEvoImg.isHidden = false
+			nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+			let str = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+			evoLbl.text = str
+		}
 	}
 	
 	@IBAction func backBtnPressed(_ sender: UIButton) {
